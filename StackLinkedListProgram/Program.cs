8e6f19734fbcb9b9330d2af1f82720cbe355e2c9
Pavan699 <pavannakate03@gmail.com>
2021-09-22 06:56:02 -0700

    Welcome messege to Stack Program

---
+++ StackLinkedListProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackLinkedListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Stack Implementation");
+        }
+    }
+}
